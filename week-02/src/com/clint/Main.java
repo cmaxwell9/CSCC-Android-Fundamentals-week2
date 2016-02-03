a538b2d92af7291d3ee2843358b1bf3330bc7d54
Clint <cmax0460@yahoo.com>
2016-02-02 21:53:11 -0500

    Initial commit

---
+++ week-02/src/com/clint/Main.java
@@ -0,0 +1,18 @@
+package com.clint;
+
+
+public class Main {
+public static void main (String[] args)
+{
+
+    String[] weekDays = {"SUN", "MON", "Tues", "WED", "THUR", "FRI", "SAT"};
+    int[] temps = {45,29,10,22,41,28,33};
+    int[] precipitation = {95,60,25,5,0,75,90};
+
+    for (int index = 0; index < weekDays.length; index++){
+        if ((temps[index] <= 32) && (precipitation[index] > 50)){
+            System.out.println(weekDays[index]);
+        }
+    }
+}
+}
