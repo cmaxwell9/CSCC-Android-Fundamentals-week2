48ce117155f220062afc10f3053c32c29debfca6
Clint <cmax0460@yahoo.com>
2016-02-13 11:09:24 -0500

    week 4 exercise....

--- week-04/src/Main.java
+++ week-04/src/Main.java
@@ -12,10 +12,10 @@ public class Main {
         }
 
     }
-    static Map<String, Double> calculateAverages(Map<String, List> dailyTemps){
+    static Map<String, Double> calculateAverages(Map<String, List<Double>> dailyTemps){
         Map<String,Double> averages = new TreeMap<>();
         for (String city: dailyTemps.keySet()){
-            List<Double>temps = dailyTemps.get(city);
+            List<Double> temps = dailyTemps.get(city);
             double total = 0;
             for(double temp: temps){
                 total += temp;
@@ -29,31 +29,35 @@ public class Main {
 
     static Map<String, List<Double>>  getUserInput() {
         List<String> cities = new ArrayList<>();
+        Map<String, List<Double>> cityTemps = new TreeMap<>();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a city name:");
         String city = scanner.nextLine();
 
         while (!city.equals("END")) {
-            cities .add(city);
+            cities.add(city);
+            System.out.println("Enter a city name");
             city = scanner.nextLine();
 
         }
-        for (String cityName: cities) {
+        for (String cityName : cities) {
             System.out.println("Enter the temperature for the next five days for " + cityName + " (seperate each tempature with a space). ");
             String userInput = scanner.nextLine();
             String[] stringTemps = userInput.split(" ");
             List<Double> doubleTemps = new ArrayList<>();
-            for (String temp: stringTemps) {
+            for (String temp : stringTemps) {
                 doubleTemps.add(Double.parseDouble(temp));
 
             }
-            cityTemps.put(cityName,doubleTemps);
+            cityTemps.put(cityName, doubleTemps);
         }
         return cityTemps;
-        )
 
+    }
 
     public static void main(String[] args) {
-        benchmark();
+            Map<String, List<Double>> userInput = getUserInput();
+        Map<String, Double> averages = calculateAverages(userInput);
+        displayAverages(averages);
     }
 }
